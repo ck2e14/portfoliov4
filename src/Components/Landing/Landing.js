d19d213e9c9c0a7714425b48b3d19fd113384486
ck2e14 <chriswkennedy@icloud.com>
2020-08-26 15:58:37 +0100

    initial commit - landing page mostly done

---
+++ src/Components/Landing/Landing.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Landing.css'
+
+const Landing = () => {
+   return (
+      <div className="landing-container">
+
+         <div className="blue-bg"></div>
+         
+         <div className="my-name">
+            <span> Hi, I'm Chris. </span>
+         </div>
+
+         <div className="tagline">
+            I design & develop web apps to help make people's lives easier.
+         </div>
+
+      </div>
+
+   )
+}
+
+export default Landing
