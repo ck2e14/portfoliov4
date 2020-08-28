d7f81dc1220764d0475d21bbae9bdd2867dfd4c4
ck2e14 <chriswkennedy@icloud.com>
2020-08-28 03:30:29 +0100

    phrasing changes in Contact Content, content title saturation ++

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -13,13 +13,13 @@ const Contact = () => {
             <div className="content-border">
 
                <div className="contact-contents">
-                  <p>I am looking for <span>full-time opportunities in web development</span>, particularly frontend. Whilst I love working primarily with <span>JavaScript</span>, and especially <span>React</span>, I am always looking to learn new stacks and technologies.<br/><br/><span>I work hard, learn quickly, and I'm always available</span> to debug or refactor! I'm also not afraid of irregular working hours, nor do I clock-off from high-priority work just because a working day has ended.<br/>
+                  <p>I am looking for <span>full-time opportunities in web development</span>, particularly frontend. Whilst I love working primarily with <span>JavaScript</span>, and especially <span>React</span>, I am always looking to learn new stacks and technologies.<br/><br/><span>I work hard, learn quickly, and I'm always available</span> to debug or refactor! I'm also not afraid of irregular working hours, nor do I clock-off from high-priority work just because a working day has ended.<br/><br/>
                   I am available immediately, able to drive and willing to relocate from my current base in Surrey.</p>
                </div>
 
                <div className="contact-links">
 
-                  <a href="https://linkedin.com/in'/christopher-w-kennedy" target="_blank" rel="noopener noreferrer" className="contact-link">
+                  <a href="https://linkedin.com/in/christopher-w-kennedy" target="_blank" rel="noopener noreferrer" className="contact-link">
                      <img src={linkedInIcon} alt="facebookIcon" className="icon"/>
                   </a>
 
