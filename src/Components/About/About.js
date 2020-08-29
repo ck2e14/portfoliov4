23ef92ed76bb592674c67beca0744f4b7cb74cec
ck2e14 <chriswkennedy@icloud.com>
2020-08-29 02:20:44 +0100

    fixed anim speeds and delays

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -27,9 +27,9 @@ const About = props => {
          <div className="content-border">
 
             <div className="about-blurb text">
-                  I am a junior <span> full-stack web developer.</span> I love to dream-up and develop <span>performant,</span> <span>powerful</span> & <span>attractive user experiences</span>, particularly in React.js, supported by Rails backends.<br/><br/><br/>
+                  I am a junior <span> full-stack web developer.</span> I love to dream-up and develop <span>performant,</span> <span>powerful</span> & <span>attractive user experiences</span>, particularly in React.js, supported by Rails backends.<br/><br/>
                   
-                  Since graduating from FlatIron's immersive software engineering course in January, I've <span>levelled-up</span> my full-stack and UX skillset through personal & commercial projects, online tutorials, forum chats and algorithm challenge websites like <a href="http://edabit.com" target="_blank" rel="noopener noreferrer" className="link">edabit.com.</a> <br/><br/><br/>
+                  Since graduating from FlatIron's immersive software engineering course in January, I've <span>levelled-up</span> my full-stack and UX skillset through personal & commercial projects, online tutorials, forum chats and algorithm challenge websites like <a href="http://edabit.com" target="_blank" rel="noopener noreferrer" className="link">edabit.com.</a> <br/><br/>
                   I am quietly <span>confident,</span> naturally <span>curious,</span> and <span>always learning</span> new skills and new technologies.
             </div>
 
